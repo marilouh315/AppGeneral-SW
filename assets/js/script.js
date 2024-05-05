128a88309fe1104d59b192c518375f5fc49fc2bb
marilouh315 <111771090+marilouh315@users.noreply.github.com>
2024-05-04 23:38:16 -0400

    nousvausx ajous

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,43 @@
-function ajouterUtilisateur() {
-    fetch(`https://restcountries.com/v3.1/translation/${input_pays}`)
+
+/**
+ * Fonction pour ajouter un utilisateur avec l'API
+ */
+let formulaire = document.getElementById("form");
+formulaire.addEventListener("submit", ajouterUtilisateur); // Pas de parenthèses
+
+function ajouterUtilisateur(event) {
+    event.preventDefault();
+
+    console.log("Ajout d'un utilisateur");
+
+    
+    const input_nom = document.getElementById("nom").value;
+    const input_courriel = document.getElementById("courriel").value;
+    const input_mot_de_passe = document.getElementById("mot_de_passe").value;
+
+    let body_utilisateur = {
+        nom_utilisateur:"nemo",
+        courriel_utilisateur:"nemo@swim.com",
+        motDePasse_utilisateur:"nemo"
+    };
+
+    console.log(body_utilisateur);
+
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body_utilisateur)
+    };
+
+    fetch(`https://app-taches.onrender.com/api/users/`, requestOptions)
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        console.log(data[0]);
-
-    });
+        if (data) {
+            console.log(data[0]);
+        }
+        else {
+            console.log('Erreur lors de la création de l\'utilisateur (data)');        
+        }
+    })
 }
