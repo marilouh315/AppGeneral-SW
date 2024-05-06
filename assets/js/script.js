4232f5cc9a089a8bd64461e1706045fd4a37fd4e
marilouh315 <111771090+marilouh315@users.noreply.github.com>
2024-05-06 18:14:16 -0400

    ajouts nouv modifs 30

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,9 +16,9 @@ function ajouterUtilisateur(event) {
     const input_mot_de_passe = document.getElementById("mot_de_passe").value;
 
     let body_utilisateur = {
-        nom_utilisateur:"nemo",
-        courriel_utilisateur:"nemo@swim.com",
-        motDePasse_utilisateur:"nemo"
+        nom_utilisateur:input_nom,
+        courriel_utilisateur:input_courriel,
+        motDePasse_utilisateur:input_mot_de_passe
     };
 
     console.log(body_utilisateur);
@@ -34,7 +34,48 @@ function ajouterUtilisateur(event) {
     .then(data => {
         console.log(data);
         if (data) {
-            console.log(data[0]);
+            console.log(data.nom_utilisateur);
+            console.log(data.cle_api);
+            document.querySelector('.message_succes').innerHTML = 'Utilisateur ajouté avec succès : <br><strong>Nom d\'utilisateur : </strong>' + body_utilisateur.nom_utilisateur + '<br><strong>Courriel : </strong>' + body_utilisateur.courriel_utilisateur + '<br><strong>Clé API : </strong>' + data.utilisateur_ajoute.cleAPI;
+        }
+        else {
+            console.log('Erreur lors de la création de l\'utilisateur (data)');        
+        }
+    })
+}
+
+
+
+
+let formulaire2 = document.getElementById("form2");
+formulaire2.addEventListener("submit", genererCleAPI); // Pas de parenthèses
+
+function genererCleAPI(event) {
+    event.preventDefault();
+
+    console.log("Génération d'une clé API");
+
+    const input_courriel = document.getElementById("courriel_cle").value;
+    const input_mot_de_passe = document.getElementById("mot_de_passe_cle").value;
+
+    let body_utilisateur = {
+        courriel_utilisateur:input_courriel,
+        motDePasse_utilisateur:input_mot_de_passe
+    };
+
+    console.log(body_utilisateur);
+
+    const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body_utilisateur)
+    };
+
+    fetch(`https://app-taches.onrender.com/api/users/`, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        if (data) {
+            document.querySelector('.message_succes_generer').innerHTML = data.message;
         }
         else {
             console.log('Erreur lors de la création de l\'utilisateur (data)');        
